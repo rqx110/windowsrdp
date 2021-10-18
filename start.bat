74d5eb3873daa604315a46862a745d5a36a03b51
Qingxiao_Ren <Qingxiao_Ren@ao-inc.com.cn>
2021-10-18 10:17:34 +0800

    update

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator Fatkhur@123 /add >nul
